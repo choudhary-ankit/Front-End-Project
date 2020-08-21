5316b92265540074d61e5f96b10433243b423017
choudhary-ankit <ankit_chy@outlook.com>
2020-08-21 23:18:42 +0530

    Add side drawer and top drawer for small screen

--- daily_shows/src/Component/Navbar/HeadingBar.js
+++ daily_shows/src/Component/Navbar/HeadingBar.js
@@ -12,6 +12,18 @@ import DialogContent from '@material-ui/core/DialogContent';
 import Card from '@material-ui/core/Card';
 import axios from 'axios'
 import {Link} from 'react-router-dom';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
+import MenuIcon from '@material-ui/icons/Menu';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
 
 
@@ -24,6 +36,8 @@ export default class HeadingBar extends Component {
             setgo:"",
             data:[],
             open:false,
+            open_drawer:false,
+            open_drawer_top:false,
         }
     }
     handleChange = (e)=>{
@@ -33,29 +47,72 @@ export default class HeadingBar extends Component {
     
     }
     setData=()=>{
-       
+        this.setState({data:''})
         axios.get(`https://www.omdbapi.com/?i=tt3896198&apikey=504c060f&s=${this.state.search}`)
-        .then(response => 
-            this.setState({data: response.data.Search})  
-        )
-        .catch(err => alert("not found any movie"));
+        .then(response =>
+            this.setState({data: response.data.Search}))
+        .catch(err => {
+            if(err){
+                alert("Movie Not Found")
+                window.location.reload(false)
+            }
+        });
         this.setState({open:true})
     }
     movie_id=(imdbID)=>{
         localStorage.setItem('movie_id', imdbID);
     }
     handleClose=()=>{
+        this.setState({data:''})
         this.setState({open:false})
     }
+    handleDrawerOpen=()=>{
+        this.setState({open_drawer:true})
+    }
+    handleDrawerClose=()=>{
+        this.setState({open_drawer:false})
+    }
+    TopDrawerOpen=()=>{
+        this.setState({open_drawer_top:true})
+    }
+    TopDrawerClose=()=>{
+        this.setState({open_drawer_top:false})
+    }
     render() {
-        console.log(this.state.search)
+        console.log(this.state.data)
         return (
             <div>
                 <AppBar position="static" style={{backgroundColor:'#24292e'}}>
                     <Toolbar>
-                        {/* <IconButton edge="start" className={Style.menuButton} color="inherit" aria-label="open drawer">
+                        <IconButton color="inherit" onClick={this.handleDrawerOpen}>    
                             <MenuIcon />
-                        </IconButton> */}
+                        </IconButton>
+                        <Drawer
+                            variant="persistent"
+                            anchor="left"
+                            open={this.state.open_drawer}
+                            classes={{paper: Style.drawerPaper}}>
+                            <div className={Style.drawerHeader}>
+                                <IconButton onClick={this.handleDrawerClose}>
+                                    {this.state.open_drawer === true ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                                </IconButton>
+                            </div>
+                            <Divider/>
+                                <div className={Style.list_item}>
+                                    <List>
+                                        <Typography disabled variant="body1" style={{marginTop:"15px", fontWeight:"bold"}}>Movies News</Typography> 
+                                        <Typography variant="body1" style={{marginTop:"15px", fontWeight:"bold"}}>Top Celebs</Typography>
+                                        <Typography variant="body1" style={{marginTop:"15px", fontWeight:"bold"}}>Top TV Shows</Typography>
+                                        <Typography variant="body1" style={{marginTop:"15px", fontWeight:"bold"}}>Award & Event</Typography>   
+                                    </List>
+                                    
+                                </div>
+                            <Divider />
+                                <List className={Style.list_item}>
+                                    <Typography variant="body1" style={{marginTop:"15px", fontWeight:"bold"}}>Coutect Us</Typography>
+                                    <Typography variant="body1" style={{marginTop:"25px", color:"grey", fontWeight:"bold"}}>V-Ds.1.0.1(Beta)</Typography>
+                                </List>
+                        </Drawer>
                         <div className={Style.title}>
                             <div>
                                 <Link to="/" style={{color:"white", textDecoration:"none"}}>
@@ -70,11 +127,58 @@ export default class HeadingBar extends Component {
                                     <InputBase placeholder="Search Movie with Title" style={{color:'inherit',marginLeft:'50px'}} onChange={this.handleChange}/>
                                 </div>
                             </div>
+                            
                         </div>
                         <div>
-                            <Button onClick={this.setData} style={{backgroundColor:"hsla(0,0%,100%,.125)", color:"Grey", border:"solid 1px hsla(0,0%,100%,.125)", borderRadius:"5px", marginLeft:"5px", height:'35px'}} >Search</Button>
-                            <Dialog onClose={this.handleClose} aria-labelledby="customized-dialog-title" open={this.state.open}>
-                                <DialogTitle id="customized-dialog-title" onClose={this.handleClose}>Search History</DialogTitle>
+                            <div className={Style.search_btn}>
+                                <Button onClick={this.setData} style={{backgroundColor:"hsla(0,0%,100%,.125)", color:"Grey", border:"solid 1px hsla(0,0%,100%,.125)", borderRadius:"5px", marginLeft:"5px", height:'35px'}} >Search</Button>
+                            </div>
+                            <div className={Style.search_icon_small}>
+                                <IconButton color="inherit" onClick={this.TopDrawerOpen} >    
+                                    <SearchIcon />
+                                </IconButton>
+                                <div>
+                                    <Drawer
+                                        variant="persistent"
+                                        anchor="top"
+                                        open={this.state.open_drawer_top}
+                                        className={ Style.drawerPaper_top}>
+                                        <div className={Style.topDrawerArrn}>
+                                            <div>
+                                                <IconButton onClick={this.TopDrawerClose}>
+                                                    {this.state.open_drawer_top === true ? <KeyboardArrowUpIcon style={{color:"white"}}/> : <KeyboardArrowDownIcon />}
+                                                </IconButton>
+                                            </div>
+                                            <div>
+                                                <div className={Style.searchDrawerArrn}>
+                                                    <div className={Style.searchDrawerTop}>
+                                                        <div className={Style.searchIcon}>
+                                                            <SearchIcon />
+                                                        </div>
+                                                        <div>
+                                                            <InputBase placeholder="Search Movie with Title" style={{color:'inherit',marginLeft:'50px'}} onChange={this.handleChange}/>
+                                                        </div>
+                                                    </div> 
+                                                    <div className={Style.search_btn_drawer_top}>
+                                                        <Button onClick={this.setData} style={{backgroundColor:"hsla(0,0%,100%,.125)", color:"Grey", border:"solid 1px hsla(0,0%,100%,.125)", borderRadius:"5px", marginLeft:"5px", height:'35px'}} >Search</Button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </Drawer>
+                                </div>
+                            </div>
+                            <Dialog fullScreen onClose={this.handleClose} aria-labelledby="customized-dialog-title" open={this.state.open}>
+                                <AppBar position="static" style={{backgroundColor:'#24292e'}}>
+                                    <Toolbar className={Style.model_appbar}>
+                                        <Link to="/" style={{color:"white", textDecoration:"none"}}>
+                                            <Typography variant="h6">DailyShow</Typography>
+                                        </Link>
+                                        <IconButton edge="start" color="inherit" onClick={this.handleClose} aria-label="close">
+                                            <CloseIcon />
+                                        </IconButton>
+                                    </Toolbar>
+                                </AppBar>
                                 <DialogContent dividers>
                                    {
                                     this.state.data.length>0?
@@ -82,26 +186,29 @@ export default class HeadingBar extends Component {
                                            return(
                                                 <Link to="/movies_info" onClick={this.handleClose} style={{color:"white", textDecoration:"none"}}>
                                                     <Button onClick={() => this.movie_id(e.imdbID)}>
-                                                        <Card style={{width:"400px"}}>
+                                                        <Card style={{width:"300px"}}>
                                                             <div className={Style.card_div}>
                                                                 <div>
                                                                     <img src={e.Poster} style={{height:"180px", width:"172px"}}></img>
                                                                 </div>
                                                                 <div className={Style.card_title}>
-                                                                    <Typography variant="h4">{e.Title}</Typography>
+                                                                    <Typography variant="h6">{e.Title}</Typography>
                                                                     <div className={Style.card_type}>
-                                                                        <Typography variant="body2">{e.Type}</Typography>
-                                                                        <Typography variant="body2">{e.Year}</Typography>
+                                                                        <Typography variant="body2">{`Type: ${e.Type}`}</Typography>
+                                                                        <Typography variant="body2">{`Year: ${e.Year}`}</Typography>
                                                                     </div>
                                                                 </div>
                                                             </div>
                                                         </Card>
                                                     </Button>
-                                                </Link>
+                                                </Link>     
                                            )
                                        })
+                                       
                                     :
-                                    <h1>This Movie is not found</h1>
+                                    <div className={Style.spiner}>
+                                        <CircularProgress disableShrink />
+                                    </div>
                                    }
                                 </DialogContent>
                             </Dialog>
